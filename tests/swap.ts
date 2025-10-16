f542d9124bd0831e149cf6a3f23670427215c988
ansh3108 <anshkumar9686@gmail.com>
2025-10-16 16:01:14 +0000

    fully completed

--- tests/swap.ts
+++ tests/swap.ts
@@ -1,16 +1,144 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Swap } from "../target/types/swap";
+import { randomBytes } from "node:crypto";
+import anchorPkg from "@coral-xyz/anchor";
+const { BN, web3, workspace, AnchorProvider, Program } = anchorPkg;
+import {
+  TOKEN_2022_PROGRAM_ID,
+  type TOKEN_PROGRAM_ID,
+  getAssociatedTokenAddressSync,
+} from "@solana/spl-token";
+import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { assert } from "chai";
+import type { Swap } from "../target/types/swap";
+import {
+  confirmTransaction,
+  createAccountsMintsAndTokenAccounts,
+  makeKeypairs,
+} from "@solana-developers/helpers";
 
-describe("swap", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const TOKEN_PROGRAM: typeof TOKEN_2022_PROGRAM_ID | typeof TOKEN_PROGRAM_ID =
+  TOKEN_2022_PROGRAM_ID;
+const SECONDS = 1000;
+const ANCHOR_SLOW_TEST_THRESHOLD = 40 * SECONDS;
 
-  const program = anchor.workspace.swap as Program<Swap>;
+const getRandomBigNumber = (size = 8) => new BN(randomBytes(size));
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+describe("swap", async () => {
+  const provider = AnchorProvider.env();
+  anchorPkg.setProvider(provider);
+
+  const user = (provider.wallet as anchorPkg.Wallet).payer;
+  const payer = user;
+  const connection = provider.connection;
+  const program = workspace.Swap as Program<Swap>;
+
+  const accounts: Record<string, PublicKey> = {
+    tokenProgram: TOKEN_PROGRAM,
+  };
+
+  let alice: web3.Keypair;
+  let bob: web3.Keypair;
+  let tokenMintA: web3.Keypair;
+  let tokenMintB: web3.Keypair;
+
+  [alice, bob, tokenMintA, tokenMintB] = makeKeypairs(4);
+
+  const tokenAOfferedAmount = new BN(1_000_000);
+  const tokenBWantedAmount = new BN(1_000_000);
+
+  before(
+    "setup users, mints, and token accounts",
+    async () => {
+      const usersMintsAndTokenAccounts =
+        await createAccountsMintsAndTokenAccounts(
+          [
+            [1_000_000_000, 0],
+            [0, 1_000_000_000],
+          ],
+          1 * LAMPORTS_PER_SOL,
+          connection,
+          payer
+        );
+
+      const users = usersMintsAndTokenAccounts.users;
+      alice = users[0];
+      bob = users[1];
+
+      const mints = usersMintsAndTokenAccounts.mints;
+      tokenMintA = mints[0];
+      tokenMintB = mints[1];
+
+      const tokenAccounts = usersMintsAndTokenAccounts.tokenAccounts;
+      const aliceTokenAccountA = tokenAccounts[0][0];
+      const aliceTokenAccountB = tokenAccounts[0][1];
+      const bobTokenAccountA = tokenAccounts[1][0];
+      const bobTokenAccountB = tokenAccounts[1][1];
+
+      accounts.maker = alice.publicKey;
+      accounts.taker = bob.publicKey;
+      accounts.tokenMintA = tokenMintA.publicKey;
+      accounts.makerTokenAccountA = aliceTokenAccountA;
+      accounts.takerTokenAccountA = bobTokenAccountA;
+      accounts.tokenMintB = tokenMintB.publicKey;
+      accounts.makerTokenAccountB = aliceTokenAccountB;
+      accounts.takerTokenAccountB = bobTokenAccountB;
+    }
+  );
+
+  it("Alice makes an offer", async () => {
+    const offerId = getRandomBigNumber();
+    const offer = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("offer"),
+        accounts.maker.toBuffer(),
+        offerId.toArrayLike(Buffer, "le", 8),
+      ],
+      program.programId
+    )[0];
+
+    const vault = getAssociatedTokenAddressSync(
+      accounts.tokenMintA,
+      offer,
+      true,
+      TOKEN_PROGRAM
+    );
+
+    accounts.offer = offer;
+    accounts.vault = vault;
+
+    const txSig = await program.methods
+      .makeOffer(offerId, tokenAOfferedAmount, tokenBWantedAmount)
+      .accounts({ ...accounts })
+      .signers([alice])
+      .rpc();
+
+    await confirmTransaction(connection, txSig);
+
+    const vaultBalanceResponse = await connection.getTokenAccountBalance(vault);
+    const vaultBalance = new BN(vaultBalanceResponse.value.amount);
+    assert(vaultBalance.eq(tokenAOfferedAmount));
+
+    const offerAccount = await program.account.offer.fetch(offer);
+    assert(offerAccount.maker.equals(alice.publicKey));
+    assert(offerAccount.tokenMintA.equals(accounts.tokenMintA));
+    assert(offerAccount.tokenMintB.equals(accounts.tokenMintB));
+    assert(offerAccount.tokenBWantedAmount.eq(tokenBWantedAmount));
+  }).slow(ANCHOR_SLOW_TEST_THRESHOLD);
+
+  it("Bob takes the offer", async () => {
+    const txSig = await program.methods
+      .takeOffer()
+      .accounts({ ...accounts })
+      .signers([bob])
+      .rpc();
+
+    await confirmTransaction(connection, txSig);
+
+    const bobBalanceResp = await connection.getTokenAccountBalance(accounts.takerTokenAccountA);
+    const bobBalance = new BN(bobBalanceResp.value.amount);
+    assert(bobBalance.eq(tokenAOfferedAmount));
+
+    const aliceBalanceResp = await connection.getTokenAccountBalance(accounts.makerTokenAccountB);
+    const aliceBalance = new BN(aliceBalanceResp.value.amount);
+    assert(aliceBalance.eq(tokenBWantedAmount));
+  }).slow(ANCHOR_SLOW_TEST_THRESHOLD);
 });
